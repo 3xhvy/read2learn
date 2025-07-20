d395ecbdede4efcab8bdcadaa14394d3b30f79ef
hohoanghvy <huyhohoang1907@gmail.com>
2025-07-20 23:07:29 +0700

    D

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/3xhvy/go-backend/internal/router"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router := router.NewServer()
+	router.Run(":8002") // listen and serve on 0.0.0.0:8080
 }
